a75f072fd2b1969ac1f7bea9626222e36f6a0503
Ryan Williams <ryanjacobwilliams@gmail.com>
2018-12-30 20:57:25 -0600

    added test to Login

---
+++ src/components/Login/Login.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Login from './Login';
+
+configure({adapter: new Adapter()});
+
+describe('<Login />', () => {
+    it('Renders without crashing', () => {
+        shallow(<Login />)
+    })
+})
